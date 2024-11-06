267c354a1ae9060d53925a632309edb7831461f2
Hafiz Abdullah Yasir <hotstolen777@gmail.com>
2024-11-06 22:06:43 +0500

    first commit

---
+++ src/Content.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Content = () => {
+  return (
+    <main>
+      <h1 className="title">Fun Facts About React</h1>
+      <ul className="titles-list">
+        <li>Was First Released in 2013</li>
+        <li>Was Originally created by jordan walke</li>
+        <li>Has Well over 100k stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
+};
+export default Content;
---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <img src="/react-logo.png" className="nav-logo" />
+        <h3>React Facts</h3>
+        <h4>React Course Project-1</h4>
+      </nav>
+    </header>
+  );
+};
+export default Header;
